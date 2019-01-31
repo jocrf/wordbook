f9f867e3518b2e4f2d2d2e5d100fb6a957254d30
Amy Frieson <ava447@gmail.com>
2019-01-31 11:29:42 -0500

    Add main container and jumbotron/home

--- wordbook/src/App/new.js
+++ wordbook/src/App/new.js
@@ -8,13 +8,13 @@ import Breadcrumb from '../Breadcrumb';
 export default class App extends Component {
   render () {
     return (
-      <React.Fragment>
+      <main className='container'>
         <h1>Wordbook App</h1>
         <Route path='/:learning?/:l(level)?/:level?/:s(section)?/:section?/:w(wordset)?/:wordset?/:e(exercise)?/:exercise?' component={Breadcrumb} />
         <Route exact path='/' component={Home} />
         <Route path='/placement' component={PlacementPage} />
         <Route path='/learning' component={LearningPage} />
-      </React.Fragment>
+      </main>
     );
   }
 }
--- wordbook/src/Home/index.js
+++ wordbook/src/Home/index.js
@@ -4,17 +4,20 @@ import { Link } from 'react-router-dom';
 export default class Home extends Component {
   render () {
     return (
-      <main>
-        <h1>
+      <div className='jumbotron'>
+        <h1 className='display-4'>
           Welcome to the Wordbook App.
         </h1>
-        <p>
-          If this is your first visit, take the <Link to='/placement'>placement quiz</Link>.
+        <hr className='my-4' />
+        <p className='lead'>
+          If this is your first visit, take the placement quiz.
         </p>
-        <p>
-          Otherwise, continue <Link to='/learning'>learning</Link>.
+        <Link to='/placement' className='btn btn-primary'>Placement Quiz</Link>
+        <p className='lead'>
+          Otherwise, continue learning.
         </p>
-      </main>
+        <Link to='/learning' className='btn btn-primary'>Select Your Level</Link>
+      </div>
     );
   }
 }
