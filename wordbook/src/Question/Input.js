9f1ef1d02c4da6861c7842345abe8cef8a84a3f1
Amy Frieson <ava447@gmail.com>
2019-02-09 17:48:34 -0500

    Add incorrectAnswer class to show score

--- wordbook/src/Question/Input.js
+++ wordbook/src/Question/Input.js
@@ -6,7 +6,9 @@ export default class Input extends Component {
     return (
       <React.Fragment>
         <input type='radio' id={option} value={option} name={prompt} checked={option === chosenAnswer} required />
-        <label htmlFor={option} className={'btn btn-outline-primary' + (showAnswers && correctAnswer === option ? ' correctAnswer' : '')}>
+        <label htmlFor={option} className={'btn btn-outline-primary' + (showAnswers && correctAnswer === option ? ' correctAnswer' : '') +
+        (showAnswers && chosenAnswer === option && correctAnswer !== option ? ' wrongAnswer' : '')
+        }>
           {/* show 'word' for review test, answer option otherwise */}
           {this.props.word ? this.props.word : option}
         </label>
