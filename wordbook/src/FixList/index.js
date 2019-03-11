a4e2d1b83c30b17d1ab75024b996958b2f705931
Amy Frieson <ava447@gmail.com>
2019-03-11 11:28:32 -0400

    Style FixList

--- wordbook/src/FixList/index.js
+++ wordbook/src/FixList/index.js
@@ -45,35 +45,33 @@ export default class FixList extends Component {
   }
 
   render () {
-    // const linkOptions = {
-    //   prefixes: 'Prefixes',
-    //   suffixes: 'Suffixes'
-    // };
     const linkText = this.props.fixType === 'prefixes' ? 'suffixes' : 'prefixes';
-    console.log(this.props);
     return (
       <React.Fragment>
-        <div>
-          <div>
-            <h2>{this.state.content.title}</h2>
-            <NavLink to={`/learning/${linkText}`}>Common {linkText[0].toUpperCase() + linkText.slice(1)}</NavLink>
+        <div className='card bg-transparent border-0'>
+          <div className='card-body'>
+            <div className='mb-3 mb-sm-0 d-sm-flex align-items-baseline'>
+              <h2 className='card-title'>{this.state.content.title}</h2>
+              <span className='d-none d-sm-block pl-sm-3 pr-sm-3'>|</span>
+              <NavLink to={`/learning/${linkText}`}>Common {linkText[0].toUpperCase() + linkText.slice(1)}</NavLink>
+            </div>
+            <p className='card-text'>{this.state.content.text}</p>
+            <ul className='list-group list-group-flush'>
+              {this.state.content.list.map(fix => (
+                <li key={fix.id} className='list-group-item'>
+                  <h3>{fix.title}</h3>
+                  <p>{fix.meaning}</p>
+                  <ul className='list-group list-group-flush'>
+                    {fix.examples.map(example => (
+                      <li key={example.word} className='list-group-item'>
+                        {example.word}: {example.def}
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
           </div>
-          <p>{this.state.content.text}</p>
-          <ul>
-            {this.state.content.list.map(fix => (
-              <li key={fix.id}>
-                <h3>{fix.title}</h3>
-                <p>{fix.meaning}</p>
-                <ul>
-                  {fix.examples.map(example => (
-                    <li key={example.word}>
-                      {example.word}: {example.def}
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
         </div>
       </React.Fragment>
     );
