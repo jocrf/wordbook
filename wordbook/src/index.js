2b3badbadd5035cf1ffd41c7d15dcad148ea6324
Amy Frieson <ava447@gmail.com>
2019-03-12 13:10:51 -0400

    Change HashRouter base

--- wordbook/src/index.js
+++ wordbook/src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { HashRouter } from 'react-router-dom';
 import './custom.scss';
 
-ReactDOM.render(<HashRouter basename='/wordbook'><App /></HashRouter>, document.getElementById('root'));
+ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
