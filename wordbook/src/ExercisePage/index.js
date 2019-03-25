63db2452f42f5e7adc9ea012a9ec5f4c6b5457a5
Amy Frieson <ava447@gmail.com>
2019-03-25 11:50:36 -0400

    Create Loading component for chapter data

--- wordbook/src/ExercisePage/index.js
+++ wordbook/src/ExercisePage/index.js
@@ -3,6 +3,7 @@ import Exercise from '../Exercise';
 import NavPanel from '../NavPanel';
 import Instructions from '../Instructions';
 import { getExercise, getInstructions, getPlacement } from '../API';
+import Loading from '../Loading';
 
 // TODO: implement showDefinition method - by default on exercise 0, optionally to click on word for other exercises
 
@@ -65,45 +66,54 @@ export default class ExercisePage extends Component {
     return (
       <section className='card'>
         <div className='card-body'>
-          {!this.state.isQuizzing &&
-            <NavPanel
-              level={this.props.level}
-              wordset={this.props.wordset}
-              section={this.props.section}
-              exercise={this.props.exercise}
-              group={this.props.group}
-              passed={this.props.passed}
-              review={this.props.review}
-              placement={this.props.placement}
-              quizCompleted={this.state.quizCompleted}
-              toggleQuizState={this.toggleQuizState}
-              instructions={this.state.instructions}
-            />
+          {
+            !this.state.chapter.definitions &&
+            <Loading />
           }
-          {this.state.isQuizzing &&
+          {
+            this.state.chapter.definitions &&
             <React.Fragment>
-              <Instructions
-                title={this.state.instructions.title}
-                instructions={this.state.instructions.instructions}
-                exampleQuestion={this.state.instructions.example}
-                exampleAnswer={this.state.instructions['example-answer']}
-                level={this.props.level}
-                wordset={this.props.wordset}
-                section={this.props.section}
-              />
-              <div className='row'>
-                <Exercise
-                  definitions={this.state.chapter.definitions}
-                  questions={this.state.chapter.exercise.questions}
-                  questionType={this.state.chapter.exercise.type}
-                  questionsToShow={this.state.chapter.exercise.type === 'mc-one' || this.state.chapter.exercise.type === 'fitb' ? 1 : null}
-                  wordlist={this.state.chapter.exercise.wordList}
-                  onQuizCompleted={this.onQuizCompleted}
-                  toggleQuizState={this.toggleQuizState}
-                  markWrongAnswers={this.props.markWrongAnswers}
+              {!this.state.isQuizzing &&
+                <NavPanel
+                  level={this.props.level}
+                  wordset={this.props.wordset}
+                  section={this.props.section}
+                  exercise={this.props.exercise}
+                  group={this.props.group}
+                  passed={this.props.passed}
+                  review={this.props.review}
                   placement={this.props.placement}
+                  quizCompleted={this.state.quizCompleted}
+                  toggleQuizState={this.toggleQuizState}
+                  instructions={this.state.instructions}
                 />
-              </div>
+              }
+              {this.state.isQuizzing &&
+                <React.Fragment>
+                  <Instructions
+                    title={this.state.instructions.title}
+                    instructions={this.state.instructions.instructions}
+                    exampleQuestion={this.state.instructions.example}
+                    exampleAnswer={this.state.instructions['example-answer']}
+                    level={this.props.level}
+                    wordset={this.props.wordset}
+                    section={this.props.section}
+                  />
+                  <div className='row'>
+                    <Exercise
+                      definitions={this.state.chapter.definitions}
+                      questions={this.state.chapter.exercise.questions}
+                      questionType={this.state.chapter.exercise.type}
+                      questionsToShow={this.state.chapter.exercise.type === 'mc-one' || this.state.chapter.exercise.type === 'fitb' ? 1 : null}
+                      wordlist={this.state.chapter.exercise.wordList}
+                      onQuizCompleted={this.onQuizCompleted}
+                      toggleQuizState={this.toggleQuizState}
+                      markWrongAnswers={this.props.markWrongAnswers}
+                      placement={this.props.placement}
+                    />
+                  </div>
+                </React.Fragment>
+              }
             </React.Fragment>
           }
         </div>
